fd0860828c7e339b3ec5a1b9f0828eb37c741af1
vsb-hub <workvb11@gmail.com>
2025-07-23 07:05:58 +0000

    conversions

---
+++ java_script/03_dt_conversion.js
@@ -0,0 +1,28 @@
+let score="33abs"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let num= Number(score)
+
+console.log(typeof (num));
+console.log( num);
+// converion jastti sctric nhi ahai javascript madhi so number neglict karaiche pan use karaicha
+
+// jar 
+// "33" -> 33
+// "33abc" -> Nan hyacha type ek numberch ahai 
+
+
+// -----------------------------------------------------
+let islogin=0;
+
+let booleanislogin=Boolean(islogin)
+console.log(typeof (booleanislogin));
+console.log( booleanislogin);
+
+// 1-> true
+// "" -> false
+// "hello" -> true
+
