8e0576675e0d782bbe70212831d5eaa603025a1f
vsb-hub <workvb11@gmail.com>
2025-07-23 06:45:19 +0000

    dtypes

---
+++ java_script/dtypes_ecms.js
@@ -0,0 +1,25 @@
+"use script"; // treats alljs code as newer version
+// alert("hello") wer using node js not browser
+
+console.log(3 + 3);
+console.log("hello");
+
+
+let name="vishal"
+let age=18
+let isLoggedin=false
+
+// number -> 2 to the power53
+// bitint
+// string->""
+// boolean ->true or false
+// null -> standalone value
+// undefined -> 
+// symbol -> unique
+// objects 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
